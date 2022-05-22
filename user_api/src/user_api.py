1609c403c06eb8af925ca50f55e4247a776608f3
Premal Singh <premals98@gmail.com>
2022-05-22 12:04:40 +0530

    Added login API for user

--- user_api/src/user_api.py
+++ user_api/src/user_api.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, request
 import pymongo
 import json
+import os
+import binascii
 
 app = Flask(__name__)
 mongo_conn = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -8,7 +10,7 @@ generic_db = mongo_conn["whatsapp_generic"]
 users_coll = generic_db["users"]
 
 @app.route('/create_user',methods = ['POST'])
-def login():
+def create_user():
     # API to create user
     req = request.get_json()
     if 'name' not in req or 'email' not in req or 'mobile' not in req:
@@ -20,6 +22,25 @@ def login():
         print("Error occured {}".format(repr(e)))
         return {"failiure": "Error occured in creating user."}
     return {"status":"success"}
+
+@app.route('/login_user',methods = ['GET'])
+def login():
+    # API to login a user
+    mobile = request.args.get('mobile')
+    if mobile is None:
+        return {"failiure":"One or more key attributes (name, email, mobile) not present in request"}
+    record = {"_id":mobile}
+    try:
+        res = users_coll.find_one(record)
+        if res:
+            token = binascii.hexlify(os.urandom(20)).decode()
+            users_coll.update_one(record, {"$set":{'token':token}})
+            return {"result":"success"}
+        else:
+            return {"result":"failiure", "reason":"Not a valid user"}
+    except Exception as e:
+        print("Error occured {}".format(repr(e)))
+        return {"failiure": "Error occured in logging in user."}
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=9000)
